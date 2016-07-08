61e4ac8db349c1aee665e7b0fb3ea086256414c5
Ed Mckain <mckain.default@gmail.com>
2016-07-08 17:50:09 -0600

    adds basic angular Js structure

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name labadeeClientApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the labadeeClientApp
+ */
+angular.module('labadeeClientApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
