78eadae9a49cb9d59adcc23893fcad164079254c
Ed Mckain <mckain.default@gmail.com>
2016-07-23 22:18:22 -0600

    fixes a couple of dependency issues.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,17 @@
  * Controller of the labadeeClientApp
  */
 angular.module('labadeeClientApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+    this.sampleText = 'Im a beautiful text!';
+
+    $scope.awesomeThings2 = [
+        'HTML5 Boilerplate',
+        'AngularJS',
+        'At&T'
+      ];
   });
